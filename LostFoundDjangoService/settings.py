1c4275abc78c7b7cd94ea0342acf74dda50f1533
yiwangchunyu <yiwangchunyu@163.com>
2019-04-19 03:13:34 +0800

    database

--- LostFoundDjangoService/settings.py
+++ LostFoundDjangoService/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'lostandfound',
-        'USER':'lostandfound',
-        'PASSWORD':'lostandfound',
+        'USER':'root',
+        'PASSWORD':'123456',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     }
